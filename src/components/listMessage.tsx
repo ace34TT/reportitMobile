3c60b4cfdfc0187ccfa55f861a04a8986043cf34
ace34TT <tafinasoa35@gmail.com>
2022-02-15 14:06:50 +0400

    test

--- src/components/listMessage.tsx
+++ src/components/listMessage.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { allMsg } from "./../webservices/messageSvc";
 
 export default function ListMessage() {
-    const [message, setmessage] = useState(
-        allMsg().then((res: any) => {
-            setmessage(res)
-        }));
-    console.log(message)
-    return (
-        <View>
-
-        </View>
-    )
+  const [message, setmessage] = useState(
+    allMsg().then((res: any) => {
+      setmessage(res);
+    })
+  );
+  console.log(message);
+  return (
+    <View>
+      <Text>Bonjour</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
